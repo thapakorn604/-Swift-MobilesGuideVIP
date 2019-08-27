cb4ac94e7a972c684d01d1ac9a2cfc757a865df6
IcedBean <thapakorn604@gmail.com>
2019-08-27 16:56:12 +0700

    first commit (create project)

---
+++ SCBFirstAssignment/Managers/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  SCBFirstAssignment
+//
+//  Created by Thapakorn Tuwaemuesa on 27/8/2562 BE.
+//  Copyright © 2562 SCB. All rights reserved.
+//
+
+import Foundation
